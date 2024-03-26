7b475bb0010fb1c4692697976691a57e5c01a9e1
Daniel Zhang <danielzhang130@gmail.com>
2024-03-25 23:15:37 -0400

    initial commit. android-14.0.0_r29

---
+++ clocks.mk
@@ -0,0 +1,12 @@
+PRODUCT_PACKAGES += \
+    SystemUIClocksOverlay
+
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-NumOverlap \
+	SystemUIClocks-Metro \
+	SystemUIClocks-Weather \
